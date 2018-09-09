545fa4bf27826aa666824b8c429b3e9e2c9e8193
Eric Middleton <ericm@iastate.edu>
2018-09-08 20:06:47 -0500

    Decreased WM8731 Line-in gain to prevent clipping

--- main/WM8731.cpp
+++ main/WM8731.cpp
@@ -35,13 +35,13 @@ void WM8731::configureDevice() {
     int err = ESP_OK;
 
     /* Left Line In - 0x00
-     * Volume: 0dB (10111) //-9dB (10001)
+     * Volume: -9dB (10001)
      * Mute: off (0)
      * Copy left data to right: true (1)
      *
-     * Value = 0b10xx10111 = 0x117 //0b10xx10001 = 0x111
+     * Value = 0b10xx10001 = 0x111
      */
-    err = writeByte(DEVICE_ADDR, 0x00, 0x117);
+    err = writeByte(DEVICE_ADDR, 0x00, 0x111);
     ESP_LOGI("INFO", "Setting Left Line In register (%d)", err);
 
     /* Left Headphone out - 0x02 (0b0000010)
